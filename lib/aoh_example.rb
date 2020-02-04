61c30064d2935f617292baed4209cb873b6e1f7f
Zac Uslianer <zacuslianer@gmail.com>
2020-02-04 23:48:40 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -14,6 +14,12 @@ end
 
 def literal_aoh
   contact_list = [
+    { name:  "Don Gately", occupation: "Live-in Staff" },
+    { name:  "Joelle van Dyne", occupation: "Radio Personality" },
+    { name:  "Pat Monteseian", occupation: "Staff" },
+    { name:  "Kate Gompert", occupation: "None" },
+    { name:  "Bruce Green", occupation: "Fan of Mildred" }
+   
     ]
 end
 

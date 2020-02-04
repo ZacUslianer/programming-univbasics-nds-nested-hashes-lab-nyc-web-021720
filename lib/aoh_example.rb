2d51b3823f5b3889d9af9cd1fcc922530396b91f
Zac Uslianer <zacuslianer@gmail.com>
2020-02-04 23:49:38 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -8,7 +8,8 @@ BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
 
 def assembled_aoh
   constant_array = [
-    DON_G, JOELLE_VD, PAT_M, KATE_G, BRUCE_G]
+    DON_G, JOELLE_VD, PAT_M, KATE_G, BRUCE_G
+    ]
   
 end
 
@@ -24,9 +25,10 @@ def literal_aoh
 end
 
 def aoh_lookup(aoh, row, key)
+  aoh[row][key]
+  
 end
 
 def aoh_update(aoh, row, key, new_value)
-  # Update the AoH data at row and key to have the value of new_value
-  # Return the updated AoH
+  aoh[row][key] = new_value
 end
